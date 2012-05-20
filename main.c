f2367748daecc7dd98ec7e78c331f203c8caf02c
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2012-05-21 04:13:18 +0900

    refactoring and .gitignore

--- main.c
+++ main.c
@@ -2,19 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INDENT               ( "\t" )
+#define INDENT               ( "        " )
 
-static int g_indent_level = 1;
+static int g_indent_level = 0;
 
 static
 void print_indent() {
   int i;
-  for (i = 0; i < g_indent_level; i++) printf(INDENT);
+  for (i = 0; i <= g_indent_level; i++) printf(INDENT);
 }
 
 static
-void emit_ptr_arith(int n, char op) {
+void print_code(const char *code) {
+  print_indent();
+  puts(code);
+}
+
+static
+void emit_ptr_arith(int n, int op) {
   char prefix = '*';
+  print_indent();
   if (op >= '<') {
     prefix = ' ';
     op = '-' - (op - '<');
@@ -22,47 +29,63 @@ void emit_ptr_arith(int n, char op) {
   }
   if (n == 1)  printf("(%c%c(%cptr));", op, op, prefix);
   else         printf("((%cptr)%c=%d);", prefix, op, n);
+  putchar('\n');
+}
+
+static
+int get_index_of(const char *s, int c) {
+  char *p = strchr(s, (char)c);
+  return p ? (int)(p - s) : -1;
 }
 
 static
 void emit_code(void) {
-  int c, n;
-  char op;
-  while ((c = getchar()) != EOF) {
-    switch (c) {
-    case '[':
-      print_indent();
-      printf("for(;*ptr;){");
-      ++g_indent_level;
-      break;
-    case ']':
-      if (--g_indent_level < 1) g_indent_level = 1;
-      print_indent();
-      putchar('}');
-      break;
-    case '+':
-    case '-':
-    case '>':
-    case '<':
-      op = (char)c;
-      n = 1;
-      for (;(c = getchar()) == op; n++);
-      ungetc(c, stdin);
-      print_indent();
-      emit_ptr_arith(n, op);
-      break;
-    case '.':
-      print_indent();
-      printf("putchar(*ptr);");
-      break;
-    case ',':
-      print_indent();
-      printf("*ptr=getchar();");
-      break;
-	default:
-      continue;
+  int c, n, op, id, commented = 0;
+  c = getchar();
+  while (c != EOF) {
+    id = get_index_of("[]+-<>.,", c);
+    if (commented && id != -1) {
+      printf(" */\n");
+      commented = 0;
     }
-    putchar('\n');
+    if (id == 0) { /* '[' */
+      print_code("for(;*ptr;){");
+      ++g_indent_level;
+      c = getchar();
+    }
+    else if (id == 1) { /* ']' */
+      if (--g_indent_level < 0) {
+        fprintf(stderr, "Error: Extra ']' found.\n");
+        return;
+      }
+      print_code("}");
+      c = getchar();
+    }
+    else if (2 <= id && id <= 5) { /* '+', '-', '<', '>' */
+      op = c;
+      n = 0;
+      for (;c == op; n++) c = getchar();
+      emit_ptr_arith(n, op);
+    }
+    else if (id == 6) { /* '.' */
+      print_code("putchar(*ptr);");
+      c = getchar();
+    }
+    else if (id == 7) { /* ',' */
+      if (commented) printf(" */\n"), commented = 0;
+      print_code("*ptr=getchar();");
+      c = getchar();
+    }
+    else { /* others */
+      if (c != '\r' && c != '\n') {
+        if (!commented) {
+          printf("/* ");
+          commented = 1;
+        }
+        putchar(c);
+      }
+      c = getchar();
+    }
   }
 }
 
@@ -116,16 +139,18 @@ int main(int argc, char *argv[]) {
   puts("  ptr_start=(byte *)calloc(mem_size, 1);");
   puts("  ptr_end=ptr_start+mem_size;");
   puts("  ptr=ptr_start;");
-  puts("// -- translated code --");
+  puts("/* -- translated code -- */");
   emit_code();
-  puts("// -- end --");
+  while (g_indent_level > 0) {
+    print_code("break;");
+    --g_indent_level;
+    print_code("}");
+    fprintf(stderr, "Warning: Incorrect Nesting.\n");
+  }
+  puts("/* -- end -- */");
   puts("putchar('\\n');");
   puts("free(ptr_start);");
   puts("return 0;");
   puts("}");
-
-  if (g_indent_level != 1) fprintf(stderr, "Warning: Incorrect Nesting.\n");
-
-  fprintf(stderr, "-- '%s' created.\n", out_filename);
   return 0;
 }
